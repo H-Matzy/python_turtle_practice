8fedc1593998ffc208faf1e931e3e58c8cefbb03
Hayden Matz <Hayden.Matz@vu.com>
2023-10-20 14:01:07 -0500

    messing with parameters

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import turtle
-from random import random
+from random import randint, random
 def recursive_pattern(t, depth, length, angle):
-    if depth == 0:
+    if depth == 0 :
         return
     else:
         r = random()
         g = random()
         b = random()
+
         # Gamer Turtle
         t.color(r,g,b)
         # Forward
@@ -14,15 +15,17 @@ def recursive_pattern(t, depth, length, angle):
 
         # Right
         t.right(angle)
-        recursive_pattern(t, depth - 1, length * 0.6, angle)  # Reduce length for smaller segments
+        recursive_pattern(t, depth - 1, length * 0.6,  angle)
 
         # Left
-        t.left(2 * angle)
-        recursive_pattern(t, depth - 1, length * 0.6, angle)  # Reduce length for smaller segments
+        t.left(2* angle)
+        recursive_pattern(t, depth - 1, length * 0.6,  angle)
+
 
         # Right
         t.right(angle)
-        recursive_pattern(t, depth - 1, length * 0.6, angle)  # Reduce length for smaller segments
+        recursive_pattern(t, depth - 1, length * 0.6,  angle)
+
 
         # Backward
         t.backward(length)
@@ -41,9 +44,10 @@ def main():
 
     depth = 8  # Adjust the depth as needed
     length = 300  # Adjust the length of each step as needed
-    angle = 70  # Adjust the angle for interesting patterns
-
-    recursive_pattern(t, depth, length, angle)
+    right_angle = randint(1,90)
+    left_angle = randint(1,90)
+    random_angle = randint(1,180)
+    recursive_pattern(t, depth, length, random_angle)
 
     window.exitonclick()
 
