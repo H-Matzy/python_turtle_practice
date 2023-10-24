9b5efaacfeec98a0e5d0d17f9832a7a20e9ba33d
Hayden Matz <Hayden.Matz@vu.com>
2023-10-24 12:40:55 -0500

    higher order turt

--- main.py
+++ main.py
@@ -1,55 +1,57 @@
 import turtle
 from random import randint, random
-def recursive_pattern(t, depth, length, angle):
-    if depth == 0 :
+
+
+def recursive_pattern(t, length, depth=8):
+    angle = randint(1, 180)
+    rgb = (random(), random(), random())
+
+    if depth == 0:
         return
     else:
-        colorValue1 = random()
-        colorValue2 = random()
-        colorValue3 = random()
-
+        depth -= 1
         # Gamer Turtle
         # Forward
         t.pendown()
-        t.color(colorValue1,colorValue2,colorValue3)
-        t.forward(length/3)
+        t.color(rgb)
+        t.forward(length / 3)
 
         t.penup()
-        t.forward(length/3)
+        t.forward(length / 3)
 
         t.pendown()
-        t.color(colorValue2,colorValue3,colorValue1)
-        t.forward(length/3)
+        t.color(rgb)
+        t.forward(length / 3)
         t.penup()
         # Right
         t.right(angle)
-        recursive_pattern(t, depth - 1, length,  angle)
+        recursive_pattern(t, length, depth - 1)
 
         # Left
-        t.left(2* angle)
-        recursive_pattern(t, depth - 1, length,  angle)
-
+        t.left(2 * angle)
+        recursive_pattern(t, length, depth - 1)
 
         # Right
         t.right(angle)
-        recursive_pattern(t, depth - 1, length,  angle)
-
+        recursive_pattern(t, length, depth - 1)
 
         # Backward
         t.backward(length)
 
-def draw_shapes(t,length, nShapes):
 
+def draw_shapes(t, length):
+    nShapes = 10
     for shape in range(0, nShapes):
 
         for turn in range(0, shape + 3):
-            r = random()
-            g = random()
-            b = random()
-            t.color(r, g, b)
+            rgb = (random(), random(), random())
+
+            t.color(rgb)
             t.forward(length)
-            t.right(360/ (shape + 3))
-def main():
+            t.right(360 / (shape + 3))
+
+
+def main(drawing_function):
     window = turtle.Screen()
     window.bgcolor("white")
     t = turtle.Turtle()
@@ -61,13 +63,10 @@ def main():
     t.pendown()
     t.setheading(180)  # Start facing up
 
-    depth = 8  # Adjust the depth as needed
     length = 200  # Adjust the length of each step as needed
-    right_angle = randint(1,90)
-    left_angle = randint(1,90)
-    random_angle = randint(1,180)
-    draw_shapes(t, length, 5)
+
+    drawing_function(t, length)
     window.exitonclick()
 
 
-main()
+main(draw_shapes)
